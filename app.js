c9c852c987180bf05f2e70f3c2df1df346f3c88a
Jabbaro <jabbarzadejamil@gmail.com>
2024-05-27 16:07:07 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,150 @@
+// let num = prompt('daxil edin');
+// if (num % 2 == 0) {
+//     console.log("cut ededir");
+// }else{
+//     console.log("tek ededir");
+// }
+
+
+// let num1 = +prompt("daxil edin")
+// let op = prompt("opereator")
+// let num2 = +prompt("daxil edin")
+
+// switch (op) {
+//     case '+':
+//       console.log(num1 + num2); 
+//         break;
+//     case '-':
+//       console.log(num1 - num2); 
+//         break;
+//     case '*':
+//       console.log(num1 * num2); 
+//         break;
+//     case '/':
+//       console.log(num1 / num2); 
+//         break;
+
+//     default:
+//         break;
+// }
+
+// let eded = +prompt("daxil edin")
+// if (eded >=1 && eded <= 50) {
+//     console.log('1 ve 50 arasindadi ');
+
+// } else {  console.log('1 ve 50 arasinda deyil')
+
+// }
+
+// let eded1=prompt("daxil edin")
+// let eded2=prompt("daxil edin")
+// if (eded1 % eded2 == 0) {
+//   alert('yes')
+// }
+// else{
+//   alert('nos')
+// }
+// let eded3=prompt ("daxil edin")
+// // let eded4 = prompt("daxil edin")
+// if ( eded3 % 2  ==0 ) {
+//   alert ("cut ")
+// }
+
+// else{ 
+//   alert("tek") 
+// }
+// let eded1=+prompt ("daxil edin")
+// let eded2=+prompt("daxil edin")
+// if (eded1 > eded2) {
+//   alert(`kicik eded:  ${eded2}`)
+// }
+// else{
+//   alert(`kicik eded:  ${eded1}`)
+// }
+
+// let eded5=-prompt ("daxil edin")
+// // let  eded6=-prompt("daxil edin")
+// if( eded<0)
+// alert("0") 
+//else
+
+// let teref = 9
+// let str = "";
+
+// for (let i = 0; i < teref; i++) {
+//   for (let j = 0; j < teref; j++) {
+//     str += '*'
+//   }
+//   str += '\n'
+// }
+
+// console.log(str);
+
+
+// let teref = 10
+
+// let str = "";
+
+// for (let i = 0; i < teref; i++) {
+
+//   for (let j = 0; j < teref; j++) {
+
+//     if (i == 0 || j == 0 || j == teref - 1 || i == teref - 1) {
+//       str += '*'
+
+//     }
+//     else {
+//       str += ' '
+//     }
+
+//   }
+//   str += "\n";
+// }
+// console.log(str);
+
+// let a = prompt("reqemi yazin")
+// let sum = 0;
+
+// for (let i = 1; i < a; i++) {
+//   if (a % i == 0) {
+//     sum += i;
+//     console.log(i);
+//   }
+
+// }
+
+// if (sum == a) {
+//   alert("mukemmel ededir")
+// } else {
+//   alert(" mukemmel deyil")
+
+// }
+
+
+// let a=prompt("reqemi yazin ")
+
+
+// for (let i = 1; i < 100; i++) {
+//   let sum = 0;
+  // for (let j = 1; j <= i / 2; j++) {
+    // if (i % j == 0) {
+      // sum += j;
+    // }
+  // }
+
+  // if (sum == i) {
+    // console.log(i + ' Mukemmel')
+  // }
+  // else {
+  // console.log(i + ' Mukemmel deyil')
+
+//   }
+// }
+ 
+
+
+
+
+
+
+
